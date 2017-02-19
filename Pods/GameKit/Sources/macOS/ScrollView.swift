8f754ea8e916f4e2d8461e0b386da6efccc6fa6e
Jaanus Siim <jaanus@jaanussiim.com>
2017-02-19 15:28:29 +0200

    Field scroll on macOS

--- Pods/GameKit/Sources/macOS/ScrollView.swift
+++ Pods/GameKit/Sources/macOS/ScrollView.swift
@@ -25,33 +25,6 @@ public class ScrollView: View {
     override var backingView: PlatformView {
         return scrollView
     }
-    internal var contained: ScrollViewContained? {
-        didSet {
-            oldValue?.backingView.removeFromSuperview()
-            oldValue?.removeFromParent()
-            
-            guard let contained = contained else {
-                return
-            }
-            
-            let sub = contained.backingView
-            flipped.addSubview(sub)
-            
-            contained.backingView.translatesAutoresizingMaskIntoConstraints = false
-            let top = NSLayoutConstraint(item: sub, attribute: .top, relatedBy: .equal, toItem: flipped, attribute: .top, multiplier: 1, constant: 0)
-            let centered = NSLayoutConstraint(item: sub, attribute: .centerX, relatedBy: .equal, toItem: flipped, attribute: .centerX, multiplier: 1, constant: 0)
-            let width = NSLayoutConstraint(item: sub, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 100)
-            let bottom = NSLayoutConstraint(item: sub, attribute: .bottom, relatedBy: .equal, toItem: flipped, attribute: .bottom, multiplier: 1, constant: 0)
-            flipped.addConstraints([top, centered, bottom, width])
-            
-            width.constant = sub.frame.width
-            widthConstraint = width
-        }
-    }
-    
-    internal var contentOffsetY: CGFloat {
-        return scrollView.contentView.visibleRect.origin.y
-    }
     private lazy var scrollView: NSScrollView = {
         let view = ShadowScrollView(frame: .zero)
         view.attached = self
@@ -64,23 +37,9 @@ public class ScrollView: View {
         
         return view
     }()
-    internal var contentSize: CGSize = .zero {
+    public var contentInset: EdgeInsets = NSEdgeInsetsZero {
         didSet {
-            widthConstraint?.constant = contentSize.width
-            flipped.frame.size.height = contentSize.height
-        }
-    }
-    public var contentInset: EdgeInsets = NSEdgeInsetsZero
-    internal var presentationInset: EdgeInsets = NSEdgeInsetsZero {
-        didSet {
-            scrollView.contentInsets = presentationInset
-
-            var presentedHeight = contained!.size.height
-            let insets = contained!.presentationInsets()
-            presentedHeight += insets.top
-            presentedHeight += insets.bottom
-            presentedHeight += contentInset.bottom
-            flipped.frame.size.height = presentedHeight
+            scrollView.contentInsets = contentInset
         }
     }
     public var verticallyCentered = false
@@ -89,113 +48,89 @@ public class ScrollView: View {
         dummy.wantsLayer = true
         return dummy
     }()
+    private var contained: ScrollViewContained? {
+        didSet {
+            oldValue?.backingView.removeFromSuperview()
+            oldValue?.removeFromParent()
+            
+            guard let contained = contained else {
+                return
+            }
+            
+            contained.color = .red
+            contained.anchorPoint = .zero
+            addChild(contained)
+            contained.inflate()
+            let backing = contained.backingView
+            backing.translatesAutoresizingMaskIntoConstraints = false
+            flipped.addSubview(backing)
+            
+            let topPinned = NSLayoutConstraint(item: backing, attribute: .top, relatedBy: .equal, toItem: flipped, attribute: .top, multiplier: 1, constant: 0)
+            let centered = NSLayoutConstraint(item: backing, attribute: .centerX, relatedBy: .equal, toItem: flipped, attribute: .centerX, multiplier: 1, constant: 0)
+            containedWidth = NSLayoutConstraint(item: backing, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 0)
+            containedHeight = NSLayoutConstraint(item: backing, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 0)
+            flipped.addConstraints([topPinned, centered, containedWidth!, containedHeight!])
+        }
+    }
+    private var containedWidth: NSLayoutConstraint?
+    private var containedHeight: NSLayoutConstraint?
     
     @objc fileprivate func didScroll(notification: NSNotification) {
         guard let object = notification.object as? NSScrollView, scrollView === object else {
             return
         }
         
-        positionPresentedNode()
-    }
-    
-    public override func positionChildren() {
-        super.positionChildren()
-        
-        contained?.presentationWidth = size.width
-        
-        if let contained = contained {
-            widthConstraint?.constant = contained.size.width
-            flipped.frame.size.height = contained.size.height
-        }
-        flipped.frame.size.width = size.width
-        
-        if verticallyCentered {
-            let spacing = max(0, (backingView.frame.size.height - contentSize.height) / 2)
-            scrollView.contentInsets = EdgeInsetsMake(spacing, 0, spacing, 0)
-        }
-
-        positionPresentedNode()
+        positionContained()
     }
     
     public override func set(color: SKColor, for attribute: Appearance.Attribute) {
         // no op
     }
     
-    internal func scroll(to point: CGPoint, animated: Bool) {
-        let saneYOffset = min(point.y, flipped.bounds.height - scrollView.bounds.height)
-        let to = CGPoint(x: 0, y: saneYOffset)
-        scrollView.contentView.scroll(to: to)
-        scrollView.reflectScrolledClipView(scrollView.contentView)
-        positionPresentedNode()
-    }
-    
     public func present(_ contained: ScrollViewContained) {
         self.contained = contained
         contained.scrollView = self
-        contained.anchorPoint = .zero
-        contentSize = contained.size
-        addChild(contained)
-        
-        contained.inflate()
-        positionPresentedNode()
     }
     
-    internal func positionPresentedNode() {
+    public func contentSizeChanged(to size: CGSize, presentationHeight: CGFloat = 0) {
+        containedWidth?.constant = size.width
+        containedHeight?.constant = size.height
+        flipped.frame.size.height = presentationHeight > 0 ? presentationHeight : size.height
+        scrollView.layoutSubtreeIfNeeded()
+        positionChildren()
+    }
+    
+    public func setContentOffset(_ offset: CGPoint, animated: Bool) {
+        scrollView.contentView.scroll(to: NSPoint(x: offset.x, y: offset.y))
+        scrollView.reflectScrolledClipView(scrollView.contentView)
+        positionContained()
+    }
+    
+    public override func positionChildren() {
+        flipped.frame.size.width = size.width
+        contained?.presentationWidth = size.width
+        positionContained()
+    }
+    
+    private func positionContained() {
         guard let contained = contained else {
             return
         }
         
-        let offset = contentOffsetY
-        let nextPosition = CGPoint(x: (size.width - contained.size.width) / 2, y: -contained.size.height + size.height + offset)
+        let visible = scrollView.documentVisibleRect
+
+        var position = contained.backingView.frame.origin
+        position.y = visible.maxY - (containedHeight?.constant ?? 0)
+        contained.position = position
+
+        let inScroll = scrollView.convert(visible, from: flipped)
         
-        let moveAction = SKAction.move(to: nextPosition, duration: 0)
-        
-        let notifyAction = SKAction.run() {
-            let bottomPoint = self.translatePointToContent(CGPoint(x: 0, y: 0))
-            let topPoint = self.translatePointToContent(CGPoint(x: 0, y: self.size.height))
-            
-            var visible = CGRect.zero
-            visible.origin = CGPoint(x: 0, y: bottomPoint.y)
-            visible.size = CGSize(width: self.contained!.size.width, height: topPoint.y - bottomPoint.y)
-            
-            var bounds = CGRect.zero
-            bounds.size = self.contained!.size
-            
-            let intersection = visible.intersection(bounds)
-            
-            // Sanity check on macOS. Exiting fullscreen gave invalid intersection
-            if CGSize.zero.equalTo(intersection.size) {
-                return
-            }
-            
-            self.contained!.scrolledVisible(to: intersection)
+        let visibleInBacking = scrollView.convert(inScroll, to: contained.backingView)
+        let combined = visibleInBacking.intersection(contained.backingView.bounds)
+        let notify = SKAction.run {
+            contained.scrolledVisible(to: combined)
         }
-        
-        let sequence = SKAction.sequence([moveAction, notifyAction])
-        contained.run(sequence)
-    }
-    
-    private func translatePointToContent(_ point: CGPoint) -> CGPoint {
-        return contained!.convert(point, from: parent!)
-    }
-    
-    public func contentSizeChanged(to size: CGSize, presentationHeight: CGFloat = 0) {
-        var insets = contentInset
-        let presentationInset = contained!.presentationInsets()
-        
-        insets.top += presentationInset.top
-        insets.bottom += presentationInset.bottom
-        insets.left += presentationInset.left
-        insets.right += presentationInset.right
-        
-        self.presentationInset = insets
-        
-        positionPresentedNode()
-    }
-    
-    public func setContentOffset(_ offset: CGPoint, animated: Bool) {
-        let saneYOffset = max(offset.y, 0)
-        scroll(to: CGPoint(x: offset.x, y: saneYOffset), animated: animated)
+        run(notify)
     }
 }
 
