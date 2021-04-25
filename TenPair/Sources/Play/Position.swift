f394813b6edceae13fb3b6b25d6b0263c27aff2f
Jaanus Siim <jaanus@jaanussiim.com>
2021-04-25 10:07:30 +0300

    Calculate tile frame

--- TenPair/Sources/Play/Position.swift
+++ TenPair/Sources/Play/Position.swift
@@ -3,11 +3,13 @@ import CoreGraphics
 
 public struct Position {
     private let itemSize: CGSize
+    private let adSize: CGSize
     private let adAfterLines: Int
     private let showingAds: Bool
     
-    public init(itemSize: CGSize, adAfterLines: Int, showingAds: Bool) {
+    public init(itemSize: CGSize, adSize: CGSize, adAfterLines: Int, showingAds: Bool) {
         self.itemSize = itemSize
+        self.adSize = adSize
         self.adAfterLines = adAfterLines
         self.showingAds = showingAds
     }
@@ -20,5 +22,16 @@ public struct Position {
         let pageLines = adAfterLines + 1
         let mod = tile.line % pageLines
         return mod == pageLines - 1
+    }
+    
+    public func frame(for tile: Tile) -> CGRect {
+        let pageLines = adAfterLines + 1
+        let onPage = tile.line / pageLines
+        let pageHeight = CGFloat(adAfterLines) * itemSize.height + adSize.height
+        let lineOnPage = tile.line % pageLines
+        let positionX = CGFloat(tile.column) * itemSize.width
+        let positionY = CGFloat(onPage) * pageHeight + CGFloat(lineOnPage) * itemSize.height
+        let itemSize = hasAd(on: tile) ? adSize : itemSize
+        return CGRect(origin: CGPoint(x: positionX, y: positionY), size: itemSize)
     }
 }
--- TenPair/Tests/PlayTests/PositionWithAdsTests.swift
+++ TenPair/Tests/PlayTests/PositionWithAdsTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import Play
 
 final class PositionWithAdsTests: XCTestCase {
-    private let position = Position(itemSize: CGSize(width: 10, height: 10), adAfterLines: 3, showingAds: true)
+    private let position = Position(itemSize: CGSize(width: 10, height: 10), adSize: CGSize(width: 90, height: 30), adAfterLines: 3, showingAds: true)
 
     // 0 |> 0, 1, 2, 3, 4, 5, 6, 7, 8
     // 1 |> 9, ...
@@ -23,5 +23,24 @@ final class PositionWithAdsTests: XCTestCase {
         XCTAssertFalse(position.hasAd(on: Tile(line: 6, column: 8)))
         XCTAssertTrue(position.hasAd(on: Tile(line: 7, column: 0)))
         XCTAssertFalse(position.hasAd(on: Tile(line: 8, column: 0)))
+    }
+    
+    func testFranes() {
+        XCTAssertEqual(CGRect(origin: .zero, size: CGSize(width: 10, height: 10)), position.frame(for: Tile(line: 0, column: 0)))
+        XCTAssertEqual(CGRect(x: 10, y: 20, width: 10, height: 10), position.frame(for: Tile(line: 2, column: 1)))
+        
+        //after first ad
+        XCTAssertEqual(CGRect(x: 20, y: 60, width: 10, height: 10), position.frame(for: Tile(line: 4, column: 2)))
+
+        //after second ad
+        XCTAssertEqual(CGRect(x: 30, y: 120, width: 10, height: 10), position.frame(for: Tile(line: 8, column: 3)))
+    }
+    
+    func testFrameOfFirstAd() {
+        XCTAssertEqual(CGRect(x: 0, y: 30, width: 90, height: 30), position.frame(for: Tile(line: 3, column: 0)))
+    }
+
+    func testFrameOfSecondAd() {
+        XCTAssertEqual(CGRect(x: 0, y: 90, width: 90, height: 30), position.frame(for: Tile(line: 7, column: 0)))
     }
 }
