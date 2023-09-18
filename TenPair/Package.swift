a6ecce9ff24d757577d9cd2e7e4d7a463297da05
Jaanus Siim <jaanus@jaanussiim.com>
2023-09-18 07:24:40 +0300

    Mark composable 1.2.0

--- TenPair/Package.swift
+++ TenPair/Package.swift
@@ -55,9 +55,9 @@ let package = Package(
         .package(url: "https://github.com/coodly/swlogger.git", exact: "0.6.1"),
         .package(url: "https://github.com/RevenueCat/purchases-ios.git", exact: "3.14.1"),
         
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "0.57.0"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "0.6.0"),
-        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.5.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "1.2.0"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.2"),
     ],
     targets: [
         .binaryTarget(
